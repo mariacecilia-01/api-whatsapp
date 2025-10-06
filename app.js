62c458dfbc9e8f91161576d5e9d34ea276f088f8
Cecilia <pereirajardimmariacecilia@gmail.com>
2025-10-05 23:54:02 -0300

    criando os endpoints

--- app.js
+++ app.js
@@ -9,3 +9,55 @@
  * npm install cors express --save
  * npm install body-parser --save 
 **************************************************************************************************************/
+
+//import das dependências da API
+const express = require('express') //responsável pela API
+const cors = require('cors') //responsável pelas permissões da API (APP)
+const bodyParser = require('body-parser') //responsável por gerenciar a chegada dos dados da APi com o frontend
+
+//import do arquivo de funções
+const dados = require('./modulo/funcoes.js')
+
+const PORT = process.PORT || 8080
+
+const app = express()
+
+app.use((request, response, next) => { 
+    response.header('Access-Control-Allow-Origin', '*') //Servidor de origem da API
+    response.header('Access-Control-Allow-Methods', 'GET') //Verbos permitidos na API -> Asterisco não funciona aqui, devemos escrever anualmente todos os verbos permitiremos
+    //carrega as configurações no CORS da API
+    app.use(cors())
+    next()//Próximo, carregar os proximos endpoints
+})
+
+//ENDPOINTS
+app.get('/v1/whats-users', function(request, response){
+    let users = dados.getAllUsers()
+
+    return response.status(users.status_code).json(users)
+})
+
+app.get('/v1/user-profile/:number', function(request, response){
+    let userNumber = request.params.number
+
+    let user = dados.getUserProfile(userNumber)
+
+    return response.status(user.status_code).json(user)
+})
+
+app.get('/v1/user-contacts/:number', function(request, response) {
+    const userNumber = request.params.number
+
+    const contatoUser = getUserContact(userNumber)
+
+    return response.status(result.status_code).json(contatoUser)
+})
+
+app.get('/v1/user-messages/:number', function(request, response) {
+    const userNumber = request.params.number
+
+    const mensagensUser = getUserMessages(userNumber)
+
+    return response.status(result.status_code).json(mensagensUser)
+})
+
